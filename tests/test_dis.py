358685f6896ba8db6458b588e757320698cdd73a
Darren Erik Vengroff <vengroff@users.noreply.github.com>
2023-03-27 15:04:40 -0400

    Improve dissimilarity index implementation (#25)

--- tests/test_dis.py
+++ tests/test_dis.py
@@ -4,7 +4,7 @@ import unittest
 
 import pandas as pd
 
-from divintseg import di, diversity, integration, segregation, dissimilarity
+import divintseg as dis
 
 
 class DfTestCase(unittest.TestCase):
@@ -23,7 +23,7 @@ class DfTestCase(unittest.TestCase):
 
     def test_diversity_of_df(self):
         """Test diversity of a dataframe."""
-        s_diversity = diversity(self.df)
+        s_diversity = dis.diversity(self.df)
 
         self.assertAlmostEqual(2.0 / 3.0, s_diversity[0], places=10)
         self.assertAlmostEqual(1.0 / 2.0, s_diversity[1], places=10)
@@ -35,7 +35,7 @@ class DfTestCase(unittest.TestCase):
 
     def test_diversity_of_sub_df(self):
         """Test diversity of a subset of the columns of a dataframe."""
-        s_diversity = diversity(self.df[["A", "C"]])
+        s_diversity = dis.diversity(self.df[["A", "C"]])
 
         self.assertAlmostEqual(0.5, s_diversity[0], places=10)
         self.assertAlmostEqual(0.0, s_diversity[1], places=10)
@@ -63,7 +63,7 @@ class IterableTestCase(unittest.TestCase):
 
     def test_array(self):
         """Test diversity of an array."""
-        div = diversity(self.a)
+        div = dis.diversity(self.a)
 
         self.assertAlmostEqual(self.div, div, places=10)
 
@@ -71,7 +71,7 @@ class IterableTestCase(unittest.TestCase):
         """Test diversity of a pandas Series."""
         s = pd.Series(self.a)
 
-        div = diversity(s)
+        div = dis.diversity(s)
 
         self.assertAlmostEqual(self.div, div, places=10)
 
@@ -109,7 +109,7 @@ class IntegrationTestCase(unittest.TestCase):
 
     def test_integration(self):
         """Test integration."""
-        df_int = integration(self.df, by="region")
+        df_int = dis.integration(self.df, by="region")
 
         for region in df_int.index:
             self.assertAlmostEqual(
@@ -120,7 +120,7 @@ class IntegrationTestCase(unittest.TestCase):
         """Test integration dropping other cols."""
         self.df["country"] = "QQQ"
 
-        df_int = integration(self.df, by="region", drop_non_numeric=True)
+        df_int = dis.integration(self.df, by="region", drop_non_numeric=True)
 
         for region in df_int.index:
             self.assertAlmostEqual(
@@ -129,7 +129,7 @@ class IntegrationTestCase(unittest.TestCase):
 
     def test_integration_drop_nothing(self):
         """Test integration dropping other cols when there are none to drop."""
-        df_int = integration(self.df, by="region", drop_non_numeric=True)
+        df_int = dis.integration(self.df, by="region", drop_non_numeric=True)
 
         for region in df_int.index:
             self.assertAlmostEqual(
@@ -138,7 +138,7 @@ class IntegrationTestCase(unittest.TestCase):
 
     def test_segregation(self):
         """Test segregation."""
-        df_seg = segregation(self.df, "region")
+        df_seg = dis.segregation(self.df, "region")
 
         for region in df_seg.index:
             self.assertAlmostEqual(
@@ -147,7 +147,7 @@ class IntegrationTestCase(unittest.TestCase):
 
     def test_di(self):
         """Test diversity and integration API."""
-        df_di = di(self.df, "region")
+        df_di = dis.di(self.df, "region")
 
         for region in df_di.index:
             self.assertAlmostEqual(
@@ -198,7 +198,7 @@ class IntegrationOverTestCase(unittest.TestCase):
 
     def test_integration_over(self):
         """Test integration."""
-        df_int = integration(self.df, by="region", over="subregion")
+        df_int = dis.integration(self.df, by="region", over="subregion")
 
         for region in df_int.index:
             self.assertAlmostEqual(
@@ -207,7 +207,7 @@ class IntegrationOverTestCase(unittest.TestCase):
 
     def test_di_over(self):
         """Test diversity and integration API."""
-        df_di = di(self.df, by="region", over="subregion")
+        df_di = dis.di(self.df, by="region", over="subregion")
 
         for region in df_di.index:
             self.assertAlmostEqual(
@@ -273,7 +273,7 @@ class IntegrationOverNonNumericTestCase(unittest.TestCase):
 
     def test_integration_region_over_subregion(self):
         """Integration at the region over subregion level."""
-        df_int = integration(
+        df_int = dis.integration(
             self.df, by="region", over="subregion", drop_non_numeric=True
         )
 
@@ -286,7 +286,7 @@ class IntegrationOverNonNumericTestCase(unittest.TestCase):
 
     def test_di_region_over_subregion(self):
         """Diversity and integration at the region over subregion level."""
-        df_di = di(self.df, by="region", over="subregion", drop_non_numeric=True)
+        df_di = dis.di(self.df, by="region", over="subregion", drop_non_numeric=True)
 
         for region in df_di.index:
             self.assertAlmostEqual(
@@ -302,7 +302,7 @@ class IntegrationOverNonNumericTestCase(unittest.TestCase):
 
     def test_integration_region_over_neighborhood(self):
         """Integration at the region over neighborhood level."""
-        df_int = integration(
+        df_int = dis.integration(
             self.df, by="region", over="neighborhood", drop_non_numeric=True
         )
 
@@ -315,7 +315,7 @@ class IntegrationOverNonNumericTestCase(unittest.TestCase):
 
     def test_di_region_over_neighborhood(self):
         """Diversity and integration at the region over neighborhood level."""
-        df_di = di(
+        df_di = dis.di(
             self.df,
             by="region",
             over="neighborhood",
@@ -357,7 +357,7 @@ class ZeroPopulationTestCase(unittest.TestCase):
         )
 
     def testDiversity(self):
-        df_di = di(self.df, by="subregion", over="region")
+        df_di = dis.di(self.df, by="subregion", over="region")
 
         df_expected = pd.DataFrame(
             [
@@ -379,7 +379,7 @@ class DissimilarityTestCase(unittest.TestCase):
 
     def test_self_dissimilarity(self):
         """A population should be perfectly similar to itself."""
-        self_dissimilarity = dissimilarity(self.df_reference, self.df_reference)
+        self_dissimilarity = dis.dissimilarity(self.df_reference, self.df_reference)
 
         self.assertEqual(1, len(self_dissimilarity))
         self.assertEqual(0.0, self_dissimilarity.iloc[0])
@@ -389,7 +389,7 @@ class DissimilarityTestCase(unittest.TestCase):
         # A smaller community with exactly the same ratios.
         df_similar = self.df_reference * 0.5
 
-        dissimilarity_index = dissimilarity(df_similar, self.df_reference)
+        dissimilarity_index = dis.dissimilarity(df_similar, self.df_reference)
 
         self.assertEqual(1, len(dissimilarity_index))
         self.assertEqual(0.0, dissimilarity_index.iloc[0])
@@ -413,7 +413,7 @@ class DissimilarityTestCase(unittest.TestCase):
                 columns=self.df_reference.columns,
             )
 
-            dissimilarity_index = dissimilarity(df_test, self.df_reference)
+            dissimilarity_index = dis.dissimilarity(df_test, self.df_reference)
 
             # The error is based on the fraction of the
             # reference population in the chosen column.
@@ -425,6 +425,176 @@ class DissimilarityTestCase(unittest.TestCase):
                     dissimilarity_index.iloc[kk],
                     places=10,
                 )
+
+
+class DissimilarityCensusTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.us = dict(
+            B03002_003E=196251375,
+            B03002_004E=39994653,
+            B03002_005E=2075852,
+            B03002_006E=18184182,
+            B03002_007E=550080,
+            B03002_008E=1017604,
+            B03002_010E=827762,
+            B03002_011E=8306780,
+            B03002_013E=33709438,
+            B03002_014E=1232731,
+            B03002_015E=612762,
+            B03002_016E=237455,
+            B03002_017E=61324,
+            B03002_018E=15766310,
+            B03002_020E=6001045,
+            B03002_021E=1739955,
+        )
+
+        self.df_us = pd.DataFrame([self.us])
+
+        self.expected_dissimilarity = pd.Series(
+            [
+                0.720327,
+                0.351581,
+                0.815910,
+                0.805025,
+                0.839579,
+            ]
+        )
+
+    def test_one_group_only(self):
+        total = sum(self.us.values())
+        for k, v in self.us.items():
+            frac = v / total
+            homogenous = {key: 100 if key == k else 0 for key in self.us.keys()}
+            similarity = dis.similarity(self.df_us, homogenous)
+            print(frac, k, similarity)
+
+    def test_tract(self):
+        df_tract = pd.DataFrame(
+            [
+                dict(
+                    STATE="28",
+                    COUNTY="049",
+                    TRACT="010103",
+                    B03002_003E=722,
+                    B03002_004E=3919,
+                    B03002_005E=0,
+                    B03002_006E=8,
+                    B03002_007E=0,
+                    B03002_008E=0,
+                    B03002_010E=0,
+                    B03002_011E=1,
+                    B03002_013E=0,
+                    B03002_014E=0,
+                    B03002_015E=0,
+                    B03002_016E=0,
+                    B03002_017E=0,
+                    B03002_018E=0,
+                    B03002_020E=0,
+                    B03002_021E=0,
+                ),
+                dict(
+                    STATE="28",
+                    COUNTY="049",
+                    TRACT="010104",
+                    B03002_003E=1053,
+                    B03002_004E=1023,
+                    B03002_005E=0,
+                    B03002_006E=8,
+                    B03002_007E=0,
+                    B03002_008E=0,
+                    B03002_010E=0,
+                    B03002_011E=0,
+                    B03002_013E=74,
+                    B03002_014E=0,
+                    B03002_015E=0,
+                    B03002_016E=0,
+                    B03002_017E=0,
+                    B03002_018E=0,
+                    B03002_020E=0,
+                    B03002_021E=0,
+                ),
+                dict(
+                    STATE="28",
+                    COUNTY="049",
+                    TRACT="010201",
+                    B03002_003E=171,
+                    B03002_004E=3029,
+                    B03002_005E=31,
+                    B03002_006E=0,
+                    B03002_007E=0,
+                    B03002_008E=0,
+                    B03002_010E=0,
+                    B03002_011E=0,
+                    B03002_013E=0,
+                    B03002_014E=8,
+                    B03002_015E=0,
+                    B03002_016E=0,
+                    B03002_017E=0,
+                    B03002_018E=0,
+                    B03002_020E=0,
+                    B03002_021E=0,
+                ),
+                dict(
+                    STATE="28",
+                    COUNTY="049",
+                    TRACT="010202",
+                    B03002_003E=318,
+                    B03002_004E=4797,
+                    B03002_005E=14,
+                    B03002_006E=12,
+                    B03002_007E=0,
+                    B03002_008E=0,
+                    B03002_010E=0,
+                    B03002_011E=31,
+                    B03002_013E=0,
+                    B03002_014E=0,
+                    B03002_015E=0,
+                    B03002_016E=0,
+                    B03002_017E=0,
+                    B03002_018E=0,
+                    B03002_020E=0,
+                    B03002_021E=0,
+                ),
+                dict(
+                    STATE="28",
+                    COUNTY="049",
+                    TRACT="010203",
+                    B03002_003E=85,
+                    B03002_004E=2341,
+                    B03002_005E=0,
+                    B03002_006E=0,
+                    B03002_007E=0,
+                    B03002_008E=14,
+                    B03002_010E=0,
+                    B03002_011E=0,
+                    B03002_013E=0,
+                    B03002_014E=0,
+                    B03002_015E=0,
+                    B03002_016E=0,
+                    B03002_017E=0,
+                    B03002_018E=0,
+                    B03002_020E=0,
+                    B03002_021E=0,
+                ),
+            ]
+        )
+        leaves = [col for col in df_tract.columns if col.startswith("B03002")]
+
+        # Try with both a dictionary and a dataframe.
+        for reference in (self.us, self.df_us):
+            dissimilarity = dis.dissimilarity(
+                df_communities=df_tract[leaves], reference=reference
+            )
+
+            for ds1, ds2 in zip(self.expected_dissimilarity, dissimilarity):
+                self.assertAlmostEqual(ds1, ds2, places=5)
+
+            similarity = dis.similarity(
+                df_communities=df_tract[leaves], reference=reference
+            )
+
+            for ds1, sim in zip(self.expected_dissimilarity, similarity):
+                self.assertAlmostEqual(1.0 - ds1, sim, places=5)
 
 
 if __name__ == "__main__":
