710ad443a6f0f698a43afff3fe58b033cfe6fdaf
Darren Erik Vengroff <vengroff@users.noreply.github.com>
2023-11-28 14:31:54 -0500

    Fix mock import for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import mock
+import unittest.mock as mock
 
 import os
 import sys
